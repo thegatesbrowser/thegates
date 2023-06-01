43cce4cb30da84a2ebe15443be0ac0719f27784e
Nordup <nordup.ondr@gmail.com>
2023-06-01 03:33:40 +0300

    don't download config and classes

--- project/the_gates/scripts/loading/gate_loader.gd
+++ project/the_gates/scripts/loading/gate_loader.gd
@@ -18,7 +18,5 @@ func load_gate(config_url: String) -> void:
 	var gate = Gate.create(config_url, c_gate.title, c_gate.description, image_path, "", "", "")
 	gate_events.gate_info_loaded_emit(gate)
 	
-	gate.godot_config = await FileDownloader.download(c_gate.godot_config_url)
-	gate.global_script_class = await FileDownloader.download(c_gate.global_script_class_url)
 	gate.resource_pack = await FileDownloader.download(c_gate.resource_pack_url)
 	gate_events.gate_loaded_emit(gate)

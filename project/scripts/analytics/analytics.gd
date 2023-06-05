992ccc93076a2e5afda07a09632b19f07323efe5
Nordup <nordup.ondr@gmail.com>
2023-06-05 22:26:33 +0300

    delete the_gates folder

---
+++ project/scripts/analytics/analytics.gd
@@ -0,0 +1,52 @@
+extends Node
+#class_name Analitycs
+
+var backend := preload("res://resources/backend.tres")
+var analytics_senders = [
+	AnalyticsSenderError.new(),
+	AnalyticsSenderGate.new(),
+	AnalyticsSenderApp.new()
+]
+
+
+func _ready() -> void:
+	await get_user_id()
+	for sender in analytics_senders:
+		add_child(sender)
+
+
+func send_event(body: Dictionary = {}) -> void:
+	var url = backend.analytics_event
+	var callback = func(_result, code, _headers, _body):
+		if code != 200: Debug.logerr("Request send_event failed. Code " + str(code))
+	
+	var err = await request(url, callback, body, HTTPClient.METHOD_POST)
+	if err != HTTPRequest.RESULT_SUCCESS: Debug.logerr("Cannot send request send_event")
+
+
+func get_user_id() -> void:
+	var url = backend.get_user_id + OS.get_unique_id()
+	var callback = func(_result, code, _headers, body):
+		if code == 200:
+			AnalyticsEvents.user_id = body.get_string_from_utf8()
+		else: Debug.logerr("Request get_user_id failed. Code " + str(code))
+	
+	var err = await request(url, callback)
+	if err != HTTPRequest.RESULT_SUCCESS: Debug.logerr("Cannot send request get_user_id")
+
+
+func request(url: String, callback: Callable,
+		body: Dictionary = {}, method: int = HTTPClient.METHOD_GET) -> Error:
+	var data = JSON.stringify(body)
+	var headers = []
+	
+	var http = HTTPRequest.new()
+	http.use_threads = true
+	add_child(http)
+	
+	var err = http.request(url, headers, method, data)
+	var res = await http.request_completed
+	callback.call(res[0], res[1], res[2], res[3])
+	remove_child(http)
+	
+	return err
---
+++ project/scripts/analytics/analytics_sender_gate.gd
@@ -0,0 +1,51 @@
+extends Node
+class_name AnalyticsSenderGate
+
+var gate_events := preload("res://resources/gate_events.res")
+
+var gate_open_time: int
+var gate_url: String
+
+
+func _ready() -> void:
+	gate_events.search_pressed.connect(send_search_press)
+	gate_events.open_gate.connect(send_gate_open)
+	gate_events.gate_entered.connect(send_gate_enter)
+	gate_events.exit_gate.connect(send_gate_exit)
+	
+	# Send latest exit event
+	var json: String = DataSaver.get_string("analytics", "send_gate_exit")
+	if json.is_empty(): return
+	DataSaver.set_value("analytics", "send_gate_exit", "")
+	Analytics.send_event(JSON.parse_string(json))
+
+
+func send_search_press(url: String) -> void:
+	Analytics.send_event(AnalyticsEvents.search_press(url))
+
+
+func send_gate_open(url: String) -> void:
+	gate_url = url
+	gate_open_time = int(Time.get_ticks_msec() / 1000)
+	Analytics.send_event(AnalyticsEvents.gate_open(url))
+
+
+func send_gate_enter() -> void:
+	var download_time = int(Time.get_ticks_msec() / 1000) - gate_open_time
+	gate_open_time = int(Time.get_ticks_msec() / 1000)
+	Analytics.send_event(AnalyticsEvents.gate_enter(gate_url, download_time))
+
+
+func send_gate_exit() -> void:
+	var time_spend = int(Time.get_ticks_msec() / 1000) - gate_open_time
+	Analytics.send_event(AnalyticsEvents.gate_exit(gate_url, time_spend))
+	gate_url = ""
+
+
+func _exit_tree() -> void:
+	if gate_url.is_empty(): return
+	
+	# Save to send on open
+	var time_spend = int(Time.get_ticks_msec() / 1000) - gate_open_time
+	var event = AnalyticsEvents.gate_exit(gate_url, time_spend)
+	DataSaver.set_value("analytics", "send_gate_exit", JSON.stringify(event))

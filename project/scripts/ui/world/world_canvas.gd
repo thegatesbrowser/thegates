8c726d4033cd4f71923c223da02b69ff559a1849
Nordup <nordup.ondr@gmail.com>
2024-03-24 20:51:50 +0400

    fix WorldCanvas scale animation

--- project/scripts/ui/world/world_canvas.gd
+++ project/scripts/ui/world/world_canvas.gd
@@ -1,20 +1,19 @@
 extends Control
 
-@export var render_result: RenderResult
 @export var interpolate: float:
 	set(value):
 		interpolate = value
 		animate(value)
 
-var initial: int = 1300
-var full_screen: int = 1920
+var initial: int
+var full_screen: int
 
 
 func _ready() -> void:
 	var viewport_width = ProjectSettings.get_setting("display/window/size/viewport_width", 1152)
 	var scale_width = float(custom_minimum_size.x) / viewport_width
 	
-	full_screen = render_result.width
+	full_screen = int(get_parent_control().size.x)
 	initial = int(full_screen * scale_width)
 	custom_minimum_size.x = initial
 	Debug.logclr("WorldCanvas initial: %d full_screen: %d" % [initial, full_screen], Color.DIM_GRAY)

262f14caeb1f5fe126f1c1fcfe8992ac196b4f25
Nordup <nordup.ondr@gmail.com>
2025-08-16 00:39:41 +0700

    print app info

--- app/scripts/debug_log/debug_log.gd
+++ app/scripts/debug_log/debug_log.gd
@@ -1,9 +1,14 @@
 extends RichTextLabel
 
+const APP_NAME: String = "TheGates"
+const WEB_SITE: String = "https://thegates.io"
+
 
 func _ready() -> void:
 	Debug.logged.connect(add_log)
 	meta_clicked.connect(on_meta_clicked)
+	
+	print_app_info()
 
 
 func add_log(msg: String) -> void:
@@ -12,3 +17,9 @@ func add_log(msg: String) -> void:
 
 func on_meta_clicked(meta) -> void:
 	OS.shell_open(str(meta))
+
+
+func print_app_info() -> void:
+	var version: String = ProjectSettings.get_setting("application/config/version")
+	var platform: String = OS.get_name()
+	Debug.logr("%s %s v%s - [url]%s[/url]" % [APP_NAME, platform, version, WEB_SITE])

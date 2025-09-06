16414d816c8acd34da367e288395087bec8addf6
Nordup <nordup.ondr@gmail.com>
2025-09-07 01:15:52 +0700

    acquire_client

--- app/scripts/networking/http_request_pooled.gd
+++ app/scripts/networking/http_request_pooled.gd
@@ -2,5 +2,5 @@ extends HTTPRequest
 class_name HTTPRequestPooled
 
 
-func _get_http_client() -> HTTPClient:
-	return HTTPClient.new()
+func _get_http_client(host: String, port: int, use_tls: bool) -> HTTPClient:
+	return HTTPClientPool.acquire_client(self, host, port, use_tls)

8f83bc03cc78fa1098a37635e44a67079bf129ea
Nordup <nordup.ondr@gmail.com>
2025-09-11 22:40:15 +0700

    set window size on windowed

--- app/scripts/ui/menu/window_buttons.gd
+++ app/scripts/ui/menu/window_buttons.gd
@@ -3,6 +3,7 @@ extends Control
 @export var minimize: BaseButton
 @export var maximize: BaseButton
 @export var exit: BaseButton
+@export var restored_window_ratio: float = 0.75
 
 
 func _ready() -> void:
@@ -20,8 +21,15 @@ func on_maximize() -> void:
 	if mode == DisplayServer.WINDOW_MODE_WINDOWED:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		restore_from_maximized()
 
 
 func on_exit() -> void:
 	get_tree().quit()
+
+
+func restore_from_maximized() -> void:
+	var usable: Rect2i = DisplayServer.screen_get_usable_rect(DisplayServer.window_get_current_screen())
+	var target_size: Vector2i = Vector2i(int(usable.size.x * restored_window_ratio), int(usable.size.y * restored_window_ratio))
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_size(target_size)
